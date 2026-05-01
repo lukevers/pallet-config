862d1f40636acd2c2b6c7ba38578b82f3bc12c77
Luke Evers <luke@lukevers.com>
2026-05-01 14:37:06 -0400

    Deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/pallet-config/' : '/',
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
